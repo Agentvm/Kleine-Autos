30e04aa00242f561dabbc1f60ecf4ad1bfdba060
Agentvm <pc.gamer@hotmail.de>
2021-08-31 19:14:21 +0200

    Added ToDo List (Please Have a Look @Flo @Lars)

---
+++ Assets/ToDo.cs
@@ -0,0 +1,21 @@
+/*
+Milestone - The First Race:
+- Splitscreen
+- Multiplayer: Enable multiple vehicles (Adapt Drive.cs and the InputModule)
+- Damage Model + Damage & Respawn of Vehicles
+
+Milestone - Advanced Gameplay:
+- Modular Racing Track
+
+Milestone - A Visual Impression:
+- Effekte (Explosion, Rauch, Reifenspuren)
+- Environment 
+  - Assets im Assetstore finden (https://assetstore.unity.com/packages/3d/environments/landscapes/free-rocks-19288?free=true&q=rocks&orderBy=1 ) oder selber bauen
+  - Wassershader
+
+ 
+ 
+ 
+ 
+ 
+ */
