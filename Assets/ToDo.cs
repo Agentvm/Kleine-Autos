f4d90c59df7d2c976dc13a858efc22ba1793636b
Agentvm <pc.gamer@hotmail.de>
2021-10-23 00:04:04 +0200

    Spielkonzept Freitag

--- Assets/ToDo.cs
+++ Assets/ToDo.cs
@@ -7,23 +7,56 @@
 
 ### Milestones
 Milestone - The First Race:
-- Splitscreen
-- Multiplayer: Enable multiple vehicles (Adapt Drive.cs and the InputModule)
-- Damage Model + Damage & Respawn of Vehicles
+- Multiplayer: Enable multiple vehicles (Adapt Drive.cs and the InputModule) <= Jannik
+- Simple Menu for Multiplayer <= Jannik
+- Semi-realistic drive physics in Drive.cs <= ?
+- Racing Logic: Who wins? What happens after a game?
 
 Milestone - Advanced Gameplay:
-- Modular Racing Track
+- Modular Racing Track <= Flo macht den Track Editor
 
 Milestone - A Visual Impression:
-- Effekte (Explosion, Rauch, Reifenspuren)
-- Environment 
-  - Assets im Assetstore finden (https://assetstore.unity.com/packages/3d/environments/landscapes/free-rocks-19288?free=true&q=rocks&orderBy=1 ) oder selber bauen
+- Effekte (Explosion, Rauch, Reifenspuren) <= Macht Laas (VFX Graph)
+- Environment
+  - Assets im Assetstore finden <= Laas
+    (https://assetstore.unity.com/packages/3d/environments/landscapes/free-rocks-19288?free=true&q=rocks&orderBy=1 ) oder selber bauen
   - Wassershader
 
- 
+Milestone - Extended Racing Gameplay
+- Stone Avalanche
+- Thunderstorm
+- Fire
+- Drawbridges
+- Need for Speed: Triggered Obstacles (Players can activate to create diversion)
+
+Milestone - LAN Multiplayer <= FloFloFloFloFlo
+- We want this game to be played in local networks. Do it, Flo!
+
+Milestone - Race Modes
+- Team (no team damage)
+- Tag (Chase the red flag, whoever carries the flag the longest, wins. Take the flag by crashing into)
+- Mayhem (most kills win)
+- Speed (first one wins)
+
 ### Fancy Ideas
 - Pinball cannon (orange press magazine filled with pinballs) that doesn't do much damage, but displaces players
+- Tankstand hat Auswirkungen auf Fahrphysik (+ Reifenbelagsphysik)
+- Strecken bei Rückrunde rückwärts fahren
 
- 
- 
+ ### Done
+- Damage Model + Damage & Respawn of Vehicles
+- Splitscreen
+
+
+
+### Style
+Assets
+- Eher MG als Pilzkanone
+- Ehr PickUpTruck als PodRacer oder Zeppelin
+- Strecken (Lava, Wüste, Eis, Unterwelt, Flussdelta, Unterwasser, Wolkenstadt, Mondkrater, Parallelstrecke)
+
+Progress
+- Jedes mal, wenn man das Spiel startet, beginnt man mit einem von zwei Standardwagen
+- Von klein (Jeep) bis fancy (Lambo) & fett (Panzer)
+- 
  */
